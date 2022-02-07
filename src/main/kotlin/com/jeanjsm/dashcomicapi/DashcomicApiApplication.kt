31039703fe25941649d3fc58e21b3c7738e638fe
Jean Silva Medeiros <jean_silvajsm@hotmail.com>
2022-02-06 22:40:16 -0400

    Initial commit

---
+++ src/main/kotlin/com/jeanjsm/dashcomicapi/DashcomicApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jeanjsm.dashcomicapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DashcomicApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DashcomicApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/jeanjsm/dashcomicapi/DashcomicApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jeanjsm.dashcomicapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DashcomicApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
