f66fc59abf1ab998eb2e478096331a8283bc3330
Jean Silva Medeiros <jean_silvajsm@hotmail.com>
2022-03-19 15:00:14 -0400

    Fix file reading

--- src/main/kotlin/com/jeanjsm/dashcomicapi/controller/vo/ComicResponseSearchVO.kt
+++ src/main/kotlin/com/jeanjsm/dashcomicapi/controller/vo/ComicResponseSearchVO.kt
@@ -4,10 +4,14 @@ import com.jeanjsm.dashcomicapi.integration.anilist.vo.Media
 
 data class ComicResponseSearchVO(
     val title: String?,
-    val coverUrl: String
+    val coverUrl: String,
+    val id: Int,
+    val idMal: Int
 ) {
     constructor(media: Media) : this(
         title = media.title.romaji ?: media.title.english,
-        coverUrl = media.coverImage.extraLarge
+        coverUrl = media.coverImage.extraLarge,
+        id = media.id,
+        idMal = media.idMal
     )
 }
--- src/main/kotlin/com/jeanjsm/dashcomicapi/util/GraphqlSchemaReaderUtil.kt
+++ src/main/kotlin/com/jeanjsm/dashcomicapi/util/GraphqlSchemaReaderUtil.kt
@@ -1,5 +1,7 @@
 package com.jeanjsm.dashcomicapi.util
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Component
@@ -11,7 +13,9 @@ class GraphqlSchemaReaderUtil {
     private lateinit var resourceLoader: ResourceLoader
 
     fun getSchemaFromFileName(fileName: String): String {
-        return resourceLoader.getResource("classpath:graphql/$fileName.graphql").file.readText()
+        val resource = javaClass.getResourceAsStream("/graphql/$fileName.graphql")
+        val reader = BufferedReader(InputStreamReader(resource))
+        return reader.readText()
     }
 
 }
